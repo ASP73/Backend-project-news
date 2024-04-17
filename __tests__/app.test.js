57895eb43ecd6e648a2befbef4e5bd9b23581929
ASP73 <alecsparker@outlook.com>
2024-04-17 10:41:46 +0100

    completes 3-api-endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,10 @@ const topics = require("../db/data/test-data/topics");
 beforeEach(() => {
     return seed(data);
 });
+
+afterAll(() => {
+    return db.end();
+ });
 //previous katas have asked for typeof but this specifies properties
 describe("/api/topics", () => {
     test("GET 200: Responds with an array of all topics", () => {
@@ -34,7 +38,28 @@ describe("/api/topics", () => {
     });
 
 
- afterAll(() => {
-    return db.end();
- });
+
+})
+//do I do a length test with just the endpoint data? Unsure, nothing in model for logic
+describe("/api", () => {
+    test("GET 200: Responds with endpoint json data", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body }) => {
+               const allEndpoints = body.endpoints;
+               expect(allEndpoints);
+                
+            });
+    });
+    test("GET 404: Returns path not found for an endpoint that does not exist", () => {
+        return request(app)
+            .get("/apx")
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.message).toEqual('Path not found');
+                            
+            });
+    });
+
 })
--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { getTopics } = require("./controllers/controller_test");
+const endpoints = require("./endpoints.json");
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/api", (req, res) => {
+    res.status(200).send({endpoints});
+});
 
 app.get("/api/topics", getTopics);
 
